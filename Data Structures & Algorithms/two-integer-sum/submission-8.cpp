f3c35ea9b27654f4f6404060c5a1385dd6bfc17a
mehmetemretopal <118732837+mehmetemretopal@users.noreply.github.com>
2026-04-25 12:28:34 +0300

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> tamamyetistin;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i+1; j <= nums.size(); j++)
+            {
+                int toplam = nums[i] + nums[j];
+                if (toplam == target)
+                {
+                    tamamyetistin.push_back(i);
+                    tamamyetistin.push_back(j);
+                    return tamamyetistin;
+                }
+            }
+        }
+        return tamamyetistin; 
+    }
+};
